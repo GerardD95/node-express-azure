9c2d7f1fff852ffa3058ab3d64ac54847722fd60
Gerard Draadjer <48570629+GerardD95@users.noreply.github.com>
2022-02-10 12:16:47 +0100

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const app = express();
 
 app.set('views', path.join(__dirname, 'views'));
 // app.engine('handlebars', exphbs({defaultLayout: 'main'}));
-app.engine('handlebars', exphbs.engine({ extname: '.hbs', defaultLayout: "main"}));
+app.engine('handlebars', exphbs.engine({ extname: 'handlebars', defaultLayout: "main"}));
 app.set('view engine', 'handlebars');
 app.set('port', config.port);
 
